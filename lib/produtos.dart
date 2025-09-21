00cd0522b68fd118c0ef4652eda8574eb26e9dcd
Lucas <lucash.arcanjo267@gmail.com>
2025-09-21 18:18:27 -0300

    Adicionando bolinhas de slide e arrumando comportamento do carousel.

--- lib/produtos.dart
+++ lib/produtos.dart
@@ -9,24 +9,115 @@ class Produtos extends StatefulWidget {
 }
 
 class ProdutosState extends State<Produtos> {
-  final List<String> imagens = ['img/sonatto_logo.png', 'img/tela-preta.png'];
+  final List<String> imagens = [
+    'img/tela-preta.png',
+    'img/tela-preta.png',
+    'img/tela-preta.png',
+    'img/tela-preta.png',
+    'img/tela-preta.png',
+    ];
+
+  int _indiceAtual = 0;
+  final CarouselSliderController _controller = CarouselSliderController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      color: Colors.white,
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.white,
           title: Image.asset('img/sonatto_logo.png', width: 200, height: 400),
         ),
         body: Center(
-          child: CarouselSlider.builder(
-            options: CarouselOptions(autoPlay: false),
-            itemCount: imagens.length,
-            itemBuilder:
-                (BuildContext context, int itemIndex, int pageViewIndex) =>
-                    Container(child: Image.asset(imagens[itemIndex].toString())),
+          child: ListView(
+            children: [
+              //Carousel para ficar passando as imagens
+              CarouselSlider.builder(
+                options: CarouselOptions(
+                  height: 500, //aqui regula a altura dele
+                  enlargeCenterPage:
+                      false, //isso se n me engano é para n esticar a imagem
+                  viewportFraction:
+                      1, //isso faz com que só uma imagem esteja na tela
+                  autoPlay: true, //esse cara faz o carousel ficar passando só
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _indiceAtual = index;
+                    });
+                  },
+                ),
+                itemCount: imagens.length,
+                carouselController: _controller,//x!
+                itemBuilder:
+                    (BuildContext context, int itemIndex, int pageViewIndex) =>
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 2,
+                              color: Colors.blueGrey,
+                            ),
+                          ),
+                          child: Stack(
+                            children: [
+                              Image.asset(
+                                imagens[itemIndex].toString(),
+                                width: double.infinity, //p cobrir a tela toda
+                              ),
+                              Positioned(
+                                top: 8,
+                                left: 8,
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 8,
+                                    vertical: 4,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    color: Colors.black54,
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  child: Text(
+                                    //texto da contagem de imagens
+                                    '${itemIndex + 1}/${imagens.length}',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+              ),
+              Row( //bolinhas q mudam conforme a pag
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:
+                    imagens.asMap().entries.map((entry) {
+                      return GestureDetector(
+                        onTap: () => _controller.animateToPage(entry.key),
+                        child: AnimatedContainer(
+                          duration: Duration(milliseconds: 300),
+                          margin: EdgeInsets.symmetric(horizontal: 4),
+                          width: _indiceAtual == entry.key ? 12 : 8,
+                          height: _indiceAtual == entry.key ? 12 : 8,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color:
+                                _indiceAtual == entry.key
+                                    ? Colors.blueAccent
+                                    : Colors.grey,
+                          ),
+                        ),
+                      );
+                    }).toList(),
+              ),
+
+            ],
           ),
         ),
+        backgroundColor: Colors.white,
       ),
     );
   }
